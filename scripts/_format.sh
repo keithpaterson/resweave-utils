b7fd45bbb30a42e77d2c7bfce3024af89f63d4e1
Keith Paterson <keith.w.paterson@gmail.com>
2024-11-28 15:27:46 -0500

    scripts: add format

---
+++ scripts/_format.sh
@@ -0,0 +1,21 @@
+using colors
+
+run_format_usage() {
+  echo "  where $(color -bold op) is:"
+  echo "    $(color -lt_green \<empty\>):  format source files"
+}
+
+run_format() {
+  local _op=$1
+  shift
+  case ${_op} in
+    "")
+      echo "Format source files ..."
+      go fmt $* ./...
+      ;;
+    *)
+      error "Unrecognized $(color -bold op): '${_op}'"
+      exit 1
+      ;;
+  esac
+}
